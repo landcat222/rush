b9e62a238cf8a5bec9c2efb6fd4c7ce680b8e255
landcat222 <67131672+landcat222@users.noreply.github.com>
2021-02-26 16:08:10 +0900

    Create base system(?)
    Enabled to output the input character string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = String::new();
+
+    print!("foo@bar ~ $ ");
+    io::stdout().flush().ok();
+    io::stdin().read_line(&mut cmd).ok();
+    println!("{}", cmd);
 }
